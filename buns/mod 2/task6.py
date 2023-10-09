05024605dbfea1b2ad6713f09f20d01a62edf46d
Marina-Kits <Marina.Kitsenko@urfu.me>
2023-10-09 15:27:36 +0500

    _

--- buns/mod 2/task6.py
+++ buns/mod 2/task6.py
@@ -1,9 +1,9 @@
-st=input()
+st=input()[::-1]
 st1=""
 for i in range(len(st)):
     if st[i]==".":
-        print(st1)
+        print(st1[::-1])
         st1=""
     else:
         st1+=st[i]
-print(st1)
+print(st1[::-1])
